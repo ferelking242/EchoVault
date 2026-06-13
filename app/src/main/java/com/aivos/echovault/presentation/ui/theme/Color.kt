58a94688341d81597ce6ad4c2c2a0e6e6a147b64
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-13 15:59:13 +0100

    Add Color.kt

---
+++ app/src/main/java/com/aivos/echovault/presentation/ui/theme/Color.kt
@@ -0,0 +1,36 @@
+package com.aivos.echovault.presentation.ui.theme
+
+  import androidx.compose.ui.graphics.Color
+
+  // Primary palette
+  val EchoViolet = Color(0xFF7C4DFF)
+  val EchoVioletDark = Color(0xFF5C35CC)
+  val EchoVioletLight = Color(0xFFB47AFF)
+  val EchoTeal = Color(0xFF00BCD4)
+  val EchoGreen = Color(0xFF4CAF50)
+  val EchoRed = Color(0xFFE53935)
+  val EchoAmber = Color(0xFFFFB300)
+  val EchoOrange = Color(0xFFFF6D00)
+
+  // Dark theme surfaces
+  val DarkBackground = Color(0xFF0D0D14)
+  val DarkSurface = Color(0xFF14141F)
+  val DarkSurfaceVariant = Color(0xFF1C1C2A)
+  val DarkCardBg = Color(0xFF1E1E2E)
+  val DarkOutline = Color(0xFF2E2E42)
+
+  // Light theme surfaces
+  val LightBackground = Color(0xFFF5F5FF)
+  val LightSurface = Color(0xFFFFFFFF)
+  val LightSurfaceVariant = Color(0xFFF0F0FF)
+  val LightCardBg = Color(0xFFFFFFFF)
+  val LightOutline = Color(0xFFDDDDEE)
+
+  // Content type colors
+  val ColorText = Color(0xFF4CAF50)
+  val ColorUrl = Color(0xFF2196F3)
+  val ColorEmail = Color(0xFFFF9800)
+  val ColorPhone = Color(0xFF9C27B0)
+  val ColorOtp = Color(0xFFE53935)
+  val ColorCode = Color(0xFF00BCD4)
+  val ColorMultiline = Color(0xFF795548)
